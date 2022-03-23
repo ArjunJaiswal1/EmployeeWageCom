4d60562ad2fe6bd6bcc209f0bd53b260a74c3f7f
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-03-24 00:02:47 +0530

    [Arjun] Add ; Added calculate monthly wages

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -61,28 +61,60 @@
 
   //UC4 Adding SwitchCase method
   
-  const present=1;
-		const parttime=2;
-		const salaryperhour=20;
-        	var workinghours;
+ // const present=1;
+	//	const parttime=2;
+	//	const salaryperhour=20;
+      //  	var workinghours;
+		//let emp = Math.floor(Math.random() * 10) %  3;
+		//switch(emp){
+          //  		case 1:
+	    	//    		console.log("Employee is present");
+              //  		workinghours=8;
+                //		break;
+            	//	case 2:
+                //		console.log("Employee is parttimer");
+                //		workinghours=4;
+                //		break;
+            	//	case 3:
+	    	    //		console.log("Employee is absent");
+                //		workinghours=0;
+                //		break;
+            	//	default:
+                //		console.log("condition doesent exist")
+		//}
+        //	const dailywage = (salaryperhour*workinghours);
+       	 //	console.log("salary will be ", dailywage);
+
+
+//UC5 Calculate Wages for a Month
+const present=1;
+        const parttime=2;
+        const salaryperhour=20;
+        var employeehours = 0;
+        var total = 0;
+        var days = 0;
+        var workinghours = 0;
+        while(days<20)
+        {
 		let emp = Math.floor(Math.random() * 10) %  3;
 		switch(emp){
-            		case 1:
-	    	    		console.log("Employee is present");
-                		workinghours=8;
-                		break;
-            		case 2:
-                		console.log("Employee is parttimer");
-                		workinghours=4;
-                		break;
-            		case 3:
-	    	    		console.log("Employee is absent");
-                		workinghours=0;
-                		break;
-            		default:
-                		console.log("condition doesent exist")
+            case 1:
+                
+                employeehours=8;
+                break;
+            case 2:
+                employeehours=4;
+                break;
+            case 3:
+                employeehours=0;
+                break;
+            default:
+                console.log("condition doesent exist")
 		}
-        	const dailywage = (salaryperhour*workinghours);
-       	 	console.log("salary will be ", dailywage);
-
-
+        const dailywage = (salaryperhour*employeehours);
+        total = (total + dailywage);
+        days = (days+1);
+        workinghours = (workinghours+employeehours);
+    }
+        console.log("Total salary will be ", total);
+        console.log("Total working hours will be ", workinghours);
