0e81d944369a14897d46cec9e3413b25f06fa230
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-03-23 23:55:38 +0530

    [Arjun] Add ; Added using switch case method

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,40 +1,88 @@
-console.log("Welcome to Employee Wage Problem \n")
-//UC1 Employee is Present or Absent.
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10) %2;
-if(empCheck == IS_ABSENT)
-{
-    console.log("Employee is absent");
-}
-else 
-{
-    console.log("Employee is present");
-}
+//console.log("Welcome to Employee Wage Problem \n")
+//nodeUC1 Employee is Present or Absent.
+//const IS_ABSENT = 0;
+//let empCheck = Math.floor(Math.random() * 10) %2;
+//if(empCheck == IS_ABSENT)
+//{
+  // console.log("Employee is absent");
+//}
+//else 
+//{
+  //  console.log("Employee is present");
+//}
 
 //UC2 Calculate Daily EmployeeWage
-console.log("UC2 Calculating wage for Employee");
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HOURS=4;
-const FULL_TIME_HOURS=8;
-const WAGE_PER_HOUR=20;
+//console.log("UC2 Calculating wage for Employee");
+//const IS_PART_TIME=1;
+//const IS_FULL_TIME=2;
+//const PART_TIME_HOURS=4;
+//const FULL_TIME_HOURS=8;
+//const WAGE_PER_HOUR=20;
 
-let empHrs=0;
-  empCheck = Math.floor(Math.random() * 10) %3;
+ //let empHrs=0;
+  //empCheck = Math.floor(Math.random() * 10) %3;
 
-switch(empCheck)
-{
-      case IS_PART_TIME:
-      empHrs=PART_TIME_HOURS;
-      break;
-      case IS_FULL_TIME:
-      empHrs=FULL_TIME_HOURS;
-      break;
-      default:
-      empHrs=0;
+//switch(empCheck)
+//{
+  //   case IS_PART_TIME:
+    // empHrs=PART_TIME_HOURS;
+      //break;
+      //case IS_FULL_TIME:
+      //empHrs=FULL_TIME_HOURS;
+      //break;
+      //default:
+      //empHrs=0;
       
-}
- empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp wage : " + empWage);
+//}
+ //empWage = empHrs * WAGE_PER_HOUR;
+//console.log("Emp wage : " + empWage);
 
-//UC3 
+//UC3 Calculate Parttime
+
+        //const present=1;
+		//const parttime=2;
+		//const salaryperhour=20;
+        //	var workinghours;
+		//let emp = Math.floor(Math.random() * 10) %  3;
+		//if(emp == present){
+	    //	    	console.log("Employee is present");
+          //      	workinghours=8;
+		//}
+		//else if (emp == parttime){
+		//	console.log("Employee is parttimer");
+          //      	workinghours=4;
+		//}
+		//else{
+		//	console.log("Employee is absent");
+          //      	workinghours=0;
+		//}
+        //	const dailywage = (salaryperhour*workinghours);
+       	 //	console.log("salary will be ", dailywage);
+
+  //UC4 Adding SwitchCase method
+  
+  const present=1;
+		const parttime=2;
+		const salaryperhour=20;
+        	var workinghours;
+		let emp = Math.floor(Math.random() * 10) %  3;
+		switch(emp){
+            		case 1:
+	    	    		console.log("Employee is present");
+                		workinghours=8;
+                		break;
+            		case 2:
+                		console.log("Employee is parttimer");
+                		workinghours=4;
+                		break;
+            		case 3:
+	    	    		console.log("Employee is absent");
+                		workinghours=0;
+                		break;
+            		default:
+                		console.log("condition doesent exist")
+		}
+        	const dailywage = (salaryperhour*workinghours);
+       	 	console.log("salary will be ", dailywage);
+
+
